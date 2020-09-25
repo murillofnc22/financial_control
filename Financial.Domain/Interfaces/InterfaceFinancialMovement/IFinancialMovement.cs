a00d874d0b2e5a3f54f6b2b31fe735eb08151603
Murillo <murillofnc@gmail.com>
2020-09-25 14:00:32 -0300

    Criando a interface IFinancialMovement no Domain

---
+++ Financial.Domain/Interfaces/InterfaceFinancialMovement/IFinancialMovement.cs
@@ -0,0 +1,9 @@
+﻿using Financial.Domain.Interfaces.Generics;
+using Financial.Entities.Entities;
+
+namespace Financial.Domain.Interfaces.InterfaceFinancialMovement
+{
+    public interface IFinancialMovement : IGeneric<FinancialMovement>
+    {
+    }
+}
--- Financial.Infra/Repository/Repositories/RepositoryFinancialMovement.cs
+++ Financial.Infra/Repository/Repositories/RepositoryFinancialMovement.cs
@@ -1,9 +1,10 @@
-﻿using Financial.Entities.Entities;
+﻿using Financial.Domain.Interfaces.InterfaceFinancialMovement;
+using Financial.Entities.Entities;
 using Financial.Infra.Repository.Generic;
 
 namespace Financial.Infra.Repository.Repositories
 {
-    public class RepositoryFinancialMovement : RespositoryGeneric<FinancialMovement>//, IFinancialMovement
+    public class RepositoryFinancialMovement : RespositoryGeneric<FinancialMovement>, IFinancialMovement
     {
     }
 }

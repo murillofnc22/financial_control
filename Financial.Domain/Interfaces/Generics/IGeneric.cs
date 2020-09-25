220af7afa2407f40c7e223c51c458aaee82cbaa7
Murillo <murillofnc@gmail.com>
2020-09-25 13:40:24 -0300

    Criando a camada de Domain e enviando ela parcial pra eu terminar a infra

---
+++ Financial.Domain/Interfaces/Generics/IGeneric.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Financial.Domain.Interfaces.Generics
+{
+    public interface IGeneric<T> where T : class
+    {
+        Task Add(T Objeto);
+        Task Update(T Objeto);
+        Task Delete(T Objeto);
+        Task<T> GetEntityById(int id);
+        Task<List<T>> List();
+    }
+}

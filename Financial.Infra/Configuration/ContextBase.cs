3a49381a63a21cf5b43da8536acb6797fa1483b9
Murillo <murillofnc@gmail.com>
2020-09-25 13:36:20 -0300

    Criando a camada de infra, não está completa, fiz na ordem errada

---
+++ Financial.Infra/Configuration/ContextBase.cs
@@ -0,0 +1,29 @@
+﻿using Financial.Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Financial.Infra.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public DbSet<FinancialMovement> FinancialMovements { get; set; }
+
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+            //Database.EnsureCreated(); <-> parece que isso aqui é pra quando usa codefirst, vou usar Migration
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(GetStringConnectionConfig());
+            base.OnConfiguring(optionsBuilder);
+        }
+        private string GetStringConnectionConfig()
+        {
+            return "Data Source=.; Initial Catalog=FINCONTROL; Integrated Security=False; User ID=sa; Password=sic742";
+        }
+    }
+}
---
+++ Financial.Infra/Repository/Generic/RespositoryGeneric.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Financial.Infra.Repository.Generic
+{
+    public class RespositoryGeneric//<T> : IGeneric<T>, IDisposable where T : class
+    {
+        //public void Dispose()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}

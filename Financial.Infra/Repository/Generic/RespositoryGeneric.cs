935313bce0a5d560198288951881d762fec96764
Murillo <murillofnc@gmail.com>
2020-09-25 13:57:08 -0300

    Criando a classe repositorygeneric que implementa a interface IGeneric do Domain

--- Financial.Infra/Repository/Generic/RespositoryGeneric.cs
+++ Financial.Infra/Repository/Generic/RespositoryGeneric.cs
@@ -1,14 +1,93 @@
-﻿using System;
+﻿using Financial.Domain.Interfaces.Generics;
+using Financial.Infra.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 namespace Financial.Infra.Repository.Generic
 {
-    public class RespositoryGeneric//<T> : IGeneric<T>, IDisposable where T : class
+    public class RespositoryGeneric<T> : IGeneric<T>, IDisposable where T : class
     {
-        //public void Dispose()
-        //{
-        //    throw new NotImplementedException();
-        //}
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+        public RespositoryGeneric()
+        {
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
+        }
+
+        public async Task Add(T Objeto)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                await data.Set<T>().AddAsync(Objeto);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete(T Objeto)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                data.Set<T>().Remove(Objeto);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task<T> GetEntityById(int id)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                return await data.Set<T>().FindAsync(id);
+            }
+        }
+
+        public async Task<List<T>> List()
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                return await data.Set<T>().AsNoTracking().ToListAsync();
+            }
+        }
+
+        public async Task Update(T Objeto)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                data.Set<T>().Update(Objeto);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        #region Disposes
+        //Flag: Has dispose already been called?
+        bool disposed = false;
+        //Instantiate a SafeHandle instance
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        //Public implementation of Dispose pattern.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        //Protected implementation of dispose pattern
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                handle.Dispose();
+                //fre any other managed objects here.
+                //
+            }
+
+            disposed = true;
+        }        
+        #endregion
     }
 }
---
+++ Financial.Infra/Repository/Repositories/RepositoryFinancialMovement.cs
@@ -0,0 +1,9 @@
+﻿using Financial.Entities.Entities;
+using Financial.Infra.Repository.Generic;
+
+namespace Financial.Infra.Repository.Repositories
+{
+    public class RepositoryFinancialMovement : RespositoryGeneric<FinancialMovement>//, IFinancialMovement
+    {
+    }
+}

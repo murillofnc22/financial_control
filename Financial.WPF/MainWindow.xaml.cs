a50314a932e7dc35101434f3fde3cd2cac2a3700
Murillo <murillofnc@gmail.com>
2020-09-25 18:32:31 -0300

    Teste de comandos - inserindo e mostrando na grid

--- Financial.WPF/MainWindow.xaml.cs
+++ Financial.WPF/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using Financial.ApplicationApp.Interfaces;
+using Financial.Entities.Entities;
+using System;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace Financial.WPF
@@ -11,12 +14,23 @@ namespace Financial.WPF
             InitializeComponent();
             this._InterfaceFinancialMovementApp = _InterfaceFinancialMovementApp;
 
-            teste();
+            RefreshGrid();
         }
 
-        private async void teste()
+        private async void RefreshGrid()
         {
-            await _InterfaceFinancialMovementApp.List();
+            DgvMovimentações.ItemsSource = await _InterfaceFinancialMovementApp.List();
+        }
+
+        private async void InsereMovimentacao()
+        {
+            await _InterfaceFinancialMovementApp.Add(new FinancialMovement() { Tipo_movimentacao = true, Valor = 150, Categoria = "Casa", Forma_pagamento = "Boleto", Data_pagamento = DateTime.Today, Data_recebimento = DateTime.Today, Data_vencimento = DateTime.Today, Taxas = 0 });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            InsereMovimentacao();
+            RefreshGrid();
         }
     }
 }
